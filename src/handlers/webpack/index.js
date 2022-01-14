d1be7018aacc3f98b69482a3b9826ee16cb5cde9
Dharmendra Negi <dharmendra.s.negi96@gmail.com>
2022-01-14 20:14:14 +0530

    webpack

--- src/handlers/webpack/index.js
+++ src/handlers/webpack/index.js
@@ -1,7 +1,7 @@
-const uuidv4 = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 exports.handler = async function (event, context) {
-  console.log(`uuid - ${uuidv4()}`);
+  console.log(`uuid - ${uuid()}`);
   return {
     statusCode: 200,
     body: JSON.stringify(
